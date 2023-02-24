76f4afd3aac39c1439f06890cebfb3c3a6bb9f06
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2023-02-24 07:46:16 +0530

    Update TwoFourWheeler.c

--- TwoFourWheeler.c
+++ TwoFourWheeler.c
@@ -5,7 +5,9 @@ int main() {
     
     scanf("%d %d", &total_vehicles, &total_wheels); //do not change this line
 
-    //Write your code here
+    two_wheelers = (total_wheels - 2*total_vehicles)/2;
+
+    four_wheelers = (4*total_vehicles - total_wheels)/2
 
     printf("%d %d\n", two_wheelers, four_wheelers); //do not change this line
   
